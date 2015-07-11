9a2d08e95c810a3e783f31207488e6698a432862
Michał Sajnóg <michal.sajnog@hotmail.com>
2015-07-11 15:37:21 +0200

    offset scrollTop fix

--- src/aos.js
+++ src/aos.js
@@ -36,8 +36,8 @@
     var _y = 0;
 
     while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
-        _x += el.offsetLeft;
-        _y += el.offsetTop;
+        _x += el.offsetLeft - (el.tagName != 'BODY' ? el.scrollTop : 0);
+        _y += el.offsetTop - (el.tagName != 'BODY' ? el.scrollLeft : 0);
         el = el.offsetParent;
     }
 
@@ -84,17 +84,17 @@
    * are right here
    */
   var handleScroll = function(){
-    scrollTop = $(window).scrollTop();
+    scrollTop = window.scrollY;
 
-    $.each(aosElementsPositions, function(i, elPos) {
-      if (scrollTop >= elPos - windowHeight) {
-        setTimeout(function(){
+    for(var i = 0; i < aosElementsPositions.length; i++) {
+      if (scrollTop >= aosElementsPositions[i] - windowHeight) {
+        // setTimeout(function(){
           $aosElements.eq(i).addClass('aos-animate');
-        }, (aosElementsDelays[i] || options.delay));
+        // }, (aosElementsDelays[i] || options.delay));
       } else {
         $aosElements.eq(i).removeClass('aos-animate');
       }
-    });
+    }
   };
 
   /**
