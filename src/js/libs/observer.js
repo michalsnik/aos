a1bdf1cc1e78a833fd69742418518097247ae52f
Michał Sajnóg <msajnog93@gmail.com>
2018-05-13 23:49:05 +0200

    Update observer.js

--- src/js/libs/observer.js
+++ src/js/libs/observer.js
@@ -26,7 +26,8 @@ function check(mutations) {
 
     const anyAOSElementAdded = addedNodes
       .concat(removedNodes)
-      .filter(el => el.hasAttribute && el.hasAttribute('data-aos')).length;
+      .filter(el => el.hasAttribute && el.hasAttribute('data-aos'))
+      .length;
 
     if (anyAOSElementAdded) {
       callback();
