94f88c64f0c45e92641c65e9db682e551699e5cc
Mike <gimp2312@gmail.com>
2018-10-12 10:35:16 +0200

    Fix: loop should end on a container

--- src/js/libs/offset.js
+++ src/js/libs/offset.js
@@ -2,17 +2,18 @@
  * Get offset of DOM element
  * like there were no transforms applied on it
  *
- * @param  {Node} el [DOM element]
- * @return {Object} [top and left offset]
+ * @param  {Node}                   el        [DOM element]
+ * @param  {(HTMLElement|Window)}   container [AOS container]
+ * @return {Object}                           [top and left offset]
  */
-const offset = function(el) {
+const offset = function(el, container) {
   let _x = 0;
   let _y = 0;
 
   while (el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
     _x += el.offsetLeft - (el.tagName != 'BODY' ? el.scrollLeft : 0);
     _y += el.offsetTop - (el.tagName != 'BODY' ? el.scrollTop : 0);
-    el = el.offsetParent;
+    el = el.offsetParent === container ? null : el.offsetParent;
   }
 
   return {
