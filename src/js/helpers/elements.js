679bfb6a6d38508cdc0f44cda7bb8145173ee266
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-02 14:13:47 +0800

    Delete src directory

--- src/js/helpers/elements.js
+++
@@ -1,9 +0,0 @@
-/**
- * Generate initial array with elements as objects
- * This array will be extended later with elements attributes values
- * like 'position'
- */
-export default () => {
-  const elements = document.querySelectorAll('[data-aos]');
-  return Array.prototype.map.call(elements, node => ({ node }));
-};
