d18267bacdd9056450fac5cf31d69f014f7a07ba
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-02 14:09:55 +0800

    Delete scripts directory

--- scripts/run-cypress-tests.js
+++
@@ -1,7 +0,0 @@
-const cypress = require('cypress');
-const server = require('./start-server');
-
-cypress.run().then(({ failures }) => {
-  server.close();
-  process.exit(failures === 0 ? 0 : 1);
-});
